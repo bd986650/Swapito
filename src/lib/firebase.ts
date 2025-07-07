4881f0456bbdf759adec068613fafbdc3bb5c671
Danil <danil@MacBook-Air-Danil.local>
2025-07-07 23:14:55 +0700

    Initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyY5BTIszHTT87fAewU5Go7i0TR-tCjGQ",
-    authDomain: "swapito-5c162.firebaseapp.com",
-    projectId: "swapito-5c162",
-    storageBucket: "swapito-5c162.firebasestorage.app",
-    messagingSenderId: "1013595719463",
-    appId: "1:1013595719463:web:0b2aa878db8ddcd16a4b02",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
